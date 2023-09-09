ae2ac151f657f29336383ba42cc04f792805dac2
imikh1991 <ilya.mikhaylyukov@gmail.com>
2023-09-09 17:07:13 +0700

    add: initial-react-config

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// eslint-disable-next-line functional/no-expression-statements
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
