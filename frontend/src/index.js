ef30a59332916aceb7880881c51abcc52342fa18
imikh1991 <ilya.mikhaylyukov@gmail.com>
2023-09-09 18:08:31 +0700

    add: start-front-back

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import init from './init.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-// eslint-disable-next-line functional/no-expression-statements
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const app = async () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(await init());
+};
+
+app();
---
+++ frontend/src/locales/index.js
@@ -0,0 +1,6 @@
+import ru from './ru';
+
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  ru,
+};
